51e79db85f1107ca31ad405f7ea1f3fa0496958f
Kishlay Kumar <kishlaykumarkkp24@gmail.com>
2023-02-02 12:54:55 +0000

    conversion operation complete

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -31,5 +31,45 @@ let someNumebr = 33;
 
 let stringNumber = String(someNumebr);
 
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+//console.log(stringNumber);
+//console.log(typeof(stringNumber));
+
+// ######################################## OPERATIONS ########################################
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Kishlay";
+let str3 = str1 + str2;
+//console.log(str3);
+
+//console.log("1" + 2);       //12
+//console.log(1 + "2");       //12
+//console.log("1" + "2");     //12
+//console.log("1" + 2 + 2);   //122   Converted according to precedence (Refer ECMA Script). 1 being a string is added to first 2 and now 12 is converted to string. Again 12 being an string is added to last 2 which gived 122 as string output.
+//console.log(1 + 2 + "2");   //32    1 being a number is added to first 2 which is also a number, therefore resulting is 3. Now this number 3 is added to last 2 which a string and returns 32 as a string output.
+
+//console.log(3 + 4 * 5 % 3); //Bad Practice. Use parenthesis. ((3+4) * (5%3)).
+
+//console.log(true);
+//console.log(+true);  //Bad Practice. Returns 1 as output. (true+ is not possible).
+
+//console.log(+""); //Returns 0. Also a bad practice.
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2+2; //Bad Practice. No consistency.
+
+let gameCounter = 100;
+
+console.log(gameCounter++);   //Returns 100. Postfix increment operator first uses the value and then increments.
+console.log(++gameCounter);   //Returns 101. Prefix increment operator first increments the value and then uses it.
